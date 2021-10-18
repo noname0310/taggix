4b029482127a9094291dfd32400179bf9dddf18d
noname <48761044+noname0310@users.noreply.github.com>
2021-10-19 00:09:51 +0900

    commit

--- src/camera.rs
+++ src/camera.rs
@@ -106,7 +106,7 @@ impl CameraController {
 
     pub fn process_keyboard(&mut self, key: VirtualKeyCode, state: ElementState) -> bool {
         let amount = if state == ElementState::Pressed {
-            1.0
+            5.0
         } else {
             0.0
         };
